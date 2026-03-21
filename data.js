c1e25a7c5e5521edecbedaea11b6ac3dc9dfd6f8
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-20 21:58:59 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-sng-li",
+      "name": "1. Resurréctio Dómini nostri Iesu Christi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20950249137904464,
+          "pitch": -0.4250589715753428,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.16823973861483843,
+          "pitch": -0.6735388679733862,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Resurréctio</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Sero autem post sabbatum, cum illucesceret in primam sabbati, venit Maria Magdalene et altera Maria videre sepulcrum. Et ecce terrae motus factus est magnus: angelus enim Domini descendit de caelo et accedens revolvit lapidem et sedebat super eum. Erat autem aspectus eius sicut fulgur, et vestimentum eius candidum sicut nix. Prae timore autem eius exterriti sunt custodes et facti sunt velut mortui. Respondens autem angelus dixit mulieribus: “ Nolite timere vos! Scio enim quod Iesum, qui crucifixus est, quaeritis. Non est hic: surrexit enim, sicut dixit. Venite, videte locum, ubi positus erat. Et cito euntes dicite discipulis eius: “Surrexit a mortuis et ecce praecedit vos in Galilaeam; ibi eum videbitis”. Ecce dixi vobis ”. Et exeuntes cito de monumento cum timore et magno gaudio cucurrerunt nuntiare discipulis eius.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et ecce Iesus occurrit illis dicens: “ Avete ”. Illae autem accesserunt et tenuerunt pedes eius et adoraverunt eum. Tunc ait illis Iesus: “ Nolite timere; ite, nuntiate fratribus meis, ut eant in Galilaeam et ibi me videbunt ”.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Matthaei 28:1–10)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-ln-tri",
+      "name": "2. Ascénsio Dómini nostri Iesu Christi in Caelum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8295190152248875,
+          "pitch": -0.16886677939099215,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        },
+        {
+          "yaw": -2.3393628150142955,
+          "pitch": 0.12362877656975968,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-sng-li"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7809236815983347,
+          "pitch": -0.5434597421898903,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Ascénsio</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Primum quidem sermonem feci de omnibus, o Theophile, quae coepit Iesus facere et docere, usque in diem, qua, cum praecepisset apostolis per Spiritum Sanctum, quos elegit, assumptus est; quibus et praebuit seipsum vivum post passionem suam in multis argumentis, per dies quadraginta apparens eis et loquens ea, quae sunt de regno Dei. Et convescens praecepit eis ab Hierosolymis ne discederent, sed exspectarent promissionem Patris: “ Quam audistis a me, quia Ioannes quidem baptizavit aqua, vos autem baptizabimini in Spiritu Sancto non post multos hos dies ”.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Igitur qui convenerant, interrogabant eum dicentes: “ Domine, si in tempore hoc restitues regnum Israeli? ”. Dixit autem eis: “ Non est vestrum nosse tempora vel momenta, quae Pater posuit in sua potestate, sed accipietis virtutem, superveniente Sancto Spiritu in vos, et eritis mihi testes et in Ierusalem et in omni Iudaea et Samaria et usque ad ultimum terrae ”.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et cum haec dixisset, videntibus illis, elevatus est, et nubes suscepit eum ab oculis eorum. Cumque intuerentur in caelum, eunte illo, ecce duo viri astiterunt iuxta illos in vestibus albis, qui et dixerunt: “ Viri Galilaei, quid statis aspicientes in caelum? Hic Iesus, qui assumptus est a vobis in caelum, sic veniet quemadmodum vidistis eum euntem in caelum ”.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Actuum Apostolorum 1:1–11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-thnh-thn-hin-xung",
+      "name": "3. Descénsus Spíritus Sancti",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07506402957144687,
+          "pitch": -0.10977309799296009,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        },
+        {
+          "yaw": 1.8835112084244283,
+          "pitch": -0.03659732029855434,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11496180811354506,
+          "pitch": -0.37224441421833276,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Descénsus Spíritus Sancti</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et cum compleretur dies Pen tecostes, erant omnes pariter in eodem loco. Et factus est repente de caelo sonus tamquam advenientis spiritus vehementis et replevit totam domum, ubi erant sedentes. Et apparuerunt illis dispertitae linguae tamquam ignis, seditque supra singulos eorum; et repleti sunt omnes Spiritu Sancto et coeperunt loqui aliis linguis, prout Spiritus dabat eloqui illis.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Erant autem in Ierusalem habitantes Iudaei, viri religiosi ex omni natione, quae sub caelo est; facta autem hac voce, convenit multitudo et confusa est, quoniam audiebat unusquisque lingua sua illos loquentes. Stupebant autem et mirabantur dicentes: “ Nonne ecce omnes isti, qui loquuntur, Galilaei sunt? Et quomodo nos audimus unusquisque propria lingua nostra, in qua nati sumus? Parthi et Medi et Elamitae et qui habitant Mesopotamiam, Iudaeam quoque et Cappadociam, Pontum et Asiam, Phrygiam quoque et Pamphyliam, Aegyptum et partes Libyae, quae est circa Cyrenem, et advenae Romani, Iudaei quoque et proselyti, Cretes et Arabes, audimus loquentes eos nostris linguis magnalia Dei ”. Stupebant autem omnes et haesitabant ad invicem dicentes: “ Quidnam hoc vult esse? ”; alii autem irridentes dicebant: “ Musto pleni sunt isti ”.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Actuum Apostolorum 2:1–13)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-tri-cho-c-b-ln-tri",
+      "name": "4. Assúmptio Beátae Maríæ Vírginis in Caelum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28290339959050925,
+          "pitch": -0.3424279478755068,
+          "rotation": 0,
+          "target": "4-5-c-cha-tri-thng-c-m-trn-tri"
+        },
+        {
+          "yaw": 2.3791351372577685,
+          "pitch": 0.184547861773261,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2414294338391887,
+          "pitch": -0.5338737219185532,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Assúmptio Beátae Maríæ Vírginis</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et ait Maria: “ Magnificat anima mea Dominum, et exsultavit spiritus meus in Deo salvatore meo,</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">quia respexit humilitatem ancillae suae. Ecce enim ex hoc beatam me dicent omnes generationes,</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">quia fecit mihi magna, qui potens est, et sanctum nomen eius,</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">et misericordia eius in progenies et progenies timentibus eum.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Fecit potentiam in brachio suo, dispersit superbos mente cordis sui;</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">deposuit potentes de sede et exaltavit humiles;</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">esurientes implevit bonis et divites dimisit inanes.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Suscepit Israel puerum suum, recordatus misericordiae, sicut locutus est ad patres nostros, Abraham et semini eius in saecula ”.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Lucae 1:46–55)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-tri-thng-c-m-trn-tri",
+      "name": "5. Coronátio Beátae Maríæ Vírginis in Caelo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.749964359811174,
+          "pitch": 0.045768055644552774,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6304062715925411,
+          "pitch": -0.20865712269064218,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Coronátio Beátae Maríæ Vírginis</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et signum magnum appa ruit in caelo: mulier amicta sole, et luna sub pedibus eius, et super caput eius corona stellarum duodecim; et in utero habens, et clamat parturiens et cruciatur, ut pariat. Et visum est aliud signum in caelo: et ecce draco rufus magnus, habens capita septem et cornua decem, et super capita sua septem diademata; et cauda eius trahit tertiam partem stellarum caeli et misit eas in terram. Et draco stetit ante mulierem, quae erat paritura, ut, cum peperisset, filium eius devoraret. Et peperit filium, masculum, qui recturus est omnes gentes in virga ferrea; et raptus est filius eius ad Deum et ad thronum eius. Et mulier fugit in desertum, ubi habet locum paratum a Deo, ut ibi pascant illam diebus mille ducentis sexaginta.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et factum est proelium in caelo, Michael et angeli eius, ut proeliarentur cum dracone. Et draco pugnavit et angeli eius, et non valuit, neque locus inventus est eorum amplius in caelo. Et proiectus est draco ille magnus, serpens antiquus, qui vocatur Diabolus et Satanas, qui seducit universum orbem; proiectus est in terram, et angeli eius cum illo proiecti sunt. Et audivi vocem magnam in caelo dicentem:</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">“ Nunc facta est salus et virtus et regnum Dei nostri et potestas Christi eius, quia proiectus est accusator fratrum nostrorum, qui accusabat illos ante conspectum Dei nostri die ac nocte. Et ipsi vicerunt illum propter sanguinem Agni et propter verbum testimonii sui; et non dilexerunt animam suam usque ad mortem. Propterea laetamini, caeli et qui habitatis in eis. Vae terrae et mari, quia descendit Diabolus ad vos habens iram magnam, sciens quod modicum tempus habet! ”.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et postquam vidit draco quod proiectus est in terram, persecutus est mulierem, quae peperit masculum. Et datae sunt mulieri duae alae aquilae magnae, ut volaret in desertum in locum suum, ubi alitur per tempus et tempora et dimidium temporis a facie serpentis. Et misit serpens ex ore suo post mulierem aquam tamquam flumen, ut eam faceret trahi a flumine. Et adiuvit terra mulierem, et aperuit terra os suum et absorbuit flumen, quod misit draco de ore suo. Et iratus est draco in mulierem et abiit facere proelium cum reliquis de semine eius, qui custodiunt mandata Dei et habent testimonium Iesu. Et stetit super arenam maris.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Apocalypsis 12:1–18)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
